0f7c41bc1ec42a9fb1e82e8a48caf164e44f7b71
samuelbaldasso <samuelbaldasso@MacBook-Pro-de-samuelbaldasso.local>
2025-12-11 10:59:40 -0300

    feat: DSA5 exercise

---
+++ src/DSA5.java
@@ -0,0 +1,30 @@
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class DSA5 {
+    public static Collection<List<String>> returningAnagrams(String[] arr){
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+        char[] charArr;
+
+        for(String a : arr) {
+            charArr = a.toCharArray();
+            Arrays.sort(charArr);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(charArr);
+
+            if(!hashMap.containsKey(sb.toString())){
+                hashMap.putIfAbsent(sb.toString(), new ArrayList<>());
+            }
+
+            hashMap.get(sb.toString()).add(a);
+        }
+        return hashMap.values();
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(returningAnagrams(arr));
+    }
+}
